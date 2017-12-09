c9cd0fff9bad06b177353b5b58506546831edba5
Jarek <jarek.kopa@gmail.com>
2017-12-09 15:58:47 +0100

    Dodano kolejne komentarze

--- hello.php
+++ hello.php
@@ -1,6 +1,11 @@
 <?php
 #to jest najprostszy możliwy komentarz
 #Komentarz musi być konkretny i nie opisywać kodu tylko go uzupełniać
+
+// komentarz c++
+
+/*koemntarz 
+multi-line*/
 echo "Hello, world!";
 ?>
 
