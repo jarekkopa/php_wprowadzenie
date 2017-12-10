6dff8f3ea9b552667a1534154fb4b53c9aef88d2
Jarek <jarek.kopa@gmail.com>
2017-12-10 11:01:45 +0100

    Dopisano dokumentacje każdej zmiennej w formacie ze slajdów

--- assign.php
+++ assign.php
@@ -2,3 +2,7 @@
 $name = 'Bobik';
 $roomsCounter = 13;
 $currentBandName = $name;
+
+echo 'Mój kot nazywa sie ' . $name . '.' . "\n";
+echo 'Hotel ma ' . $roomsCounter . ' wolnych pokoi' . "\n";
+echo 'Zespół na koncercie to ' . $currentBandName;
