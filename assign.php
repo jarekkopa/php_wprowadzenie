8f3a954f4c965cede867492a9cd7da3568132bea
Jarek <jarek.kopa@gmail.com>
2017-12-10 11:32:16 +0100

    Dopisano dokumentację do każdej zmiennej w pliku assign.php

--- assign.php
+++ assign.php
@@ -1,6 +1,21 @@
 <?php
+/**
+ * Imię psa lub kota.
+ *
+ * @var string
+ */
 $name = 'Bobik';
+/**
+ * Liczba wolnych pokoi w hotelu
+ *
+ * @var integer
+ */
 $roomsCounter = 13;
+/**
+ * Nazwa zespołu rockowego
+ *
+ * @var string
+ */
 $currentBandName = $name;
 
 echo 'Mój kot nazywa sie ' . $name . '.' . "\n";
