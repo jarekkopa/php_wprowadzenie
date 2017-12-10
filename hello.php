acaac554bb0065013cb856b135c5c5e8b1644035
Jarek <jarek.kopa@gmail.com>
2017-12-10 09:48:33 +0100

    Dodanie tekstu Hello world

--- hello.php
+++ hello.php
@@ -1,14 +1,2 @@
 <?php
-#to jest najprostszy możliwy komentarz
-#Komentarz musi być konkretny i nie opisywać kodu tylko go uzupełniać
-
-// komentarz c++
-
-/*koemntarz 
-multi-line*/
-echo "Hello, world!";
-?>
-
-To jest cześć poza PHP
-<?php
-echo "Goodbye world!";
+echo 'Hello, world!';
