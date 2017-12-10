37fb954e8c608710ce0cb4c4a44e847247ef6c27
Jarek <jarek.kopa@gmail.com>
2017-12-10 14:56:54 +0100

    Dodano plik cast_intphp

--- second.php
+++ second.php
@@ -1,6 +1,6 @@
 <?php
 $firstName = 'Jarek';
-$secondName = $firstName;
+$secondName = &$firstName;
 $secondName = 'Julian';
 echo "My first name is {$firstName}.", PHP_EOL;
 echo "My second name is {$secondName}.", PHP_EOL;
