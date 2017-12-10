eaceb4dc9e0c79b6122974298f4f9bba1991c8a8
Jarek <jarek.kopa@gmail.com>
2017-12-10 13:04:04 +0100

    uzupełniono plik ternarius.php o liczba/cyfra

--- ternarius.php
+++ ternarius.php
@@ -1,3 +1,8 @@
 <?php
-$x = 54;
-echo $x % 2 == 0 ? 'parzysta' : 'nieparzysta';
+$x = 8;
+echo $x > 9 ? 'Liczba ' : 'Cyfra ';
+echo "$x";
+echo ' ';
+echo 'jest ';
+echo $x % 2 ? 'nie' : '';
+echo 'parzysta.';
