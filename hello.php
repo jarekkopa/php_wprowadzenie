041077461fa996fa1c9a30daf49360aa37011e0f
Jarek <jarek.kopa@gmail.com>
2017-12-10 10:25:24 +0100

    Zamieniono pojedynczy apostrof na cudzysłów

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <?php
-echo 'Hello, world!';
-echo 'Oto tekst
-zlozony z kilku linii.';
-echo 'Dodajemy \'apostrof\' do stringa.';
-echo 'Znak nowej lini to \n';
-echo 'Zmienna to $zmienna';
+echo "Hello, world!";
+echo "Oto tekst
+zlozony z kilku linii.";
+echo "Dodajemy \'apostrof\' do stringa.";
+echo "Znak nowej lini to \n";
+echo "Zmienna to $zmienna";
