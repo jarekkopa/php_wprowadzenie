b819b450a29868de0b1e4bbc99a8a1a2bebba5b4
Jarek <jarek.kopa@gmail.com>
2017-12-13 00:16:03 +0100

    Utworzono plik letters.php z zadaniem ze Slacka

---
+++ letters.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Skrypt zawiera kilka ćwiczeniowych funkcji operujących na stringach.
+ *
+ * Przykład użycia:
+ * 1. Zdefiniuj zmienną $stringToTest na ciąg znaków, który chcesz przetestować.
+ * 2. Wywołuj kolejne funkcje zgodnie z ich dokumentacją.
+ * 3. Uruchom skrypt w konsoli: php -f letters.php
+ */
+
+declare(strict_types=1);
+
+$stringToTest = 'rabbit';
+removeLastLetter($stringToTest);
+appendLetters($stringToTest, 's');
+
+/**
+ * Dodanie do tekstu na końcu ciągu znaków.
+ *
+ * @param string $text Tekst, do którego dodajemy ciąg znaków.
+ * @param string $letters Ciąg znaków do dodania.
+ *
+ * @return string Tekst z dodanymi literami.
+ */
+function appendLetters(string $text, string $letters): string
+{
+    echo "Before ", __FUNCTION__, ": ", $text, PHP_EOL;
+    $text .= $letters;
+    echo "After ", __FUNCTION__, ": ", $text, PHP_EOL;
+
+    return $text;
+}
+
+/**
+ * Usunięcie ostatniej litery z podanego ciągu znaków.
+ *
+ * @param string $text Tekst, z którego usuniemy ostatnią literę.
+ *
+ * @return string Tekst bez ostatniej litery.
+ */
+function removeLastLetter(string $text): string
+{
+    echo "Before ", __FUNCTION__, ": ", $text, PHP_EOL;
+    $text = substr($text, 0, strlen($text) - 1);
+    echo "After ", __FUNCTION__, ": ", $text, PHP_EOL;
+
+    return $text;
+}
