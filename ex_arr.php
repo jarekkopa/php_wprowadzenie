bba8725086d849881658ed171e30192dcbdc394e
Jarek <jarek.kopa@gmail.com>
2017-12-16 10:21:53 +0100

    Odczytywanie z tablicy

--- ex_arr.php
+++ ex_arr.php
@@ -16,3 +16,15 @@ var_export($exArr3);
 $exArr4 = [];
 $exArr4[] = 'wartosc';
 var_export($exArr4); 
+
+$exArr5{0} = 'wartosc';
+var_export($exArr5);
+
+$wartosc = $exArr1['klucz'];
+var_export($wartosc);
+$wartosc = $exArr2[0];
+var_export($wartosc);
+$wartosc = $exArr3{'klucz'};
+var_export($wartosc);
+$wartosc = $exArr4{0};
+var_export($wartosc);
