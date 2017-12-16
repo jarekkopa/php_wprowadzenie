4700231d6cf47e3999aa541a431ced61c6e6cf67
Jarek <jarek.kopa@gmail.com>
2017-12-16 10:36:13 +0100

    Dodanie pliku weekdays.php

---
+++ weekdays.php
@@ -0,0 +1,11 @@
+<?php
+$weekdays = [
+    'monday' => 'poniedziałek',
+    'tuesday'  => 'wtorek',
+    'wednesday' => 'środa',
+    'thusday' => 'czwartek',
+    'friday' => 'piątek',
+    'saturday' => 'sobota',
+    'sunday' => 'niedziela',
+];
+var_export($weekdays);
