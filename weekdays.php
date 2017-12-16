e70d6bf96319594d2f3cff99a5f19fa9ce7d3cf4
Jarek <jarek.kopa@gmail.com>
2017-12-16 11:30:40 +0100

    Dodanie dni tygodnia po niemiecku

--- weekdays.php
+++ weekdays.php
@@ -1,11 +1,22 @@
 <?php
 $weekdays = [
-    'monday' => 'poniedziałek',
-    'tuesday'  => 'wtorek',
-    'wednesday' => 'środa',
-    'thusday' => 'czwartek',
-    'friday' => 'piątek',
-    'saturday' => 'sobota',
-    'sunday' => 'niedziela',
+    'pl' => [
+        'monday' => 'poniedziałek',
+        'tuesday'  => 'wtorek',
+        'wednesday' => 'środa',
+        'thusday' => 'czwartek',
+        'friday' => 'piątek',
+        'saturday' => 'sobota',
+        'sunday' => 'niedziela',
+    ],
+    'de' => [
+        'montag' => 'poniedziałek',
+        'dienstag'  => 'wtorek',
+        'mittwoch' => 'środa',
+        'donnerstag' => 'czwartek',
+        'freitag' => 'piątek',
+        'samstag' => 'sobota',
+        'sontag' => 'niedziela',
+    ],
 ];
 var_export($weekdays);
