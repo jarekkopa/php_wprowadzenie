50ca63fd17665bc8a23aa1fcb54842db1c803f00
Jarek <jarek.kopa@gmail.com>
2017-12-16 16:00:47 +0100

    Plik price.php

--- price.php
+++ price.php
@@ -1,5 +1,5 @@
 <?php
-$kwota = 38;
+$kwota = 58;
 
 $jednosci = ['zero', 'jeden', 'dwa', 'trzy', 'cztery', 'pięć', 'sześć', 'siedem', 'osiem', 'dziewięć'];
 //$nascie = ['dziesięć', 'jedenaście', 'dwanaście', 'trzynaście', 'czternaście', 'piętnaście', 'szesnaście', 'siedemnaście', 'osiemnaście', 'dziewiętnaście'];
@@ -9,7 +9,7 @@ if ($kwota  >= 20){
     $kwotaStr = (string)$kwota;
     $ileDziesiatki = $kwotaStr{0};
     $ileJednosci = $kwotaStr{1};
-    $dziesiat = ['dziesięć', 'dwadzieścia', 'trzydzieści', 'czterdzieści', 'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt', 'osiemdziesiąt', 'dziewięćdziesiąt'];
+    $dziesiatki = ['dziesięć', 'dwadzieścia', 'trzydzieści', 'czterdzieści', 'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt', 'osiemdziesiąt', 'dziewięćdziesiąt'];
     echo $dziesiat[$ileDziesiatki - 2], ' ' ;
 }
 echo $jednosci[$kwota] . ' ' . $zlotychKJednosci[$kwota];
