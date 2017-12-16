4edbcc1f9041c985463354166284c1b1f2d4658c
Jarek <jarek.kopa@gmail.com>
2017-12-16 09:48:56 +0100

    Dodano rozwiązanie

---
+++ casting.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Skrypt na rzutowanie z liczby zmiennoprzecinkowej na całkowitą
+ */
+$zmiennoprzecinkowa = 4.234;
+
+$calkowita = (int)$zmiennoprzecinkowa;
+
+echo $calkowita;
--- sign.php
+++ sign.php
@@ -1,4 +1,4 @@
 <?php
-$liczba = -3;
+$liczba = 2;
 
 echo $liczba > 0 ? "Liczba jest dodatnia" : "Liczba jest ujemna";
