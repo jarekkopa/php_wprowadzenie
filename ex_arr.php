e5c385278ebe850ded0df81c62b657a74ad2a9f6
Jarek <jarek.kopa@gmail.com>
2017-12-16 10:14:45 +0100

    Zapisywanie do tablicy

--- ex_arr.php
+++ ex_arr.php
@@ -3,3 +3,16 @@ $exArr1 = [
     'klucz' => 'wartosc',
 ];
 var_export($exArr1);
+
+$exArr2 = [
+    'wartosc'
+];
+var_export($exArr2);
+
+$exArr3 = [];
+$exArr3 ['klucz'] = 'wartosc';
+var_export($exArr3);
+
+$exArr4 = [];
+$exArr4[] = 'wartosc';
+var_export($exArr4); 
