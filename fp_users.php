6b6c0334e68c3a79a8c45046c972127c6e778393
Jarek <jarek.kopa@gmail.com>
2017-12-17 14:53:13 +0100

    Cwiczenie 12 - arrayfilter

---
+++ fp_users.php
@@ -0,0 +1,55 @@
+<?php
+
+$users = [
+    [
+        'name' => 'Arkadiusz',
+        'birthYear' => 1977,
+        'hobby' => 'stamps',
+        'sport' => 'basketball',
+        'occupation' => 'programmer',
+        'status' => 'hidden',
+    ],
+    [
+        'name' => 'Nadia',
+        'birthYear' => 1988,
+        'hobby' => 'sport',
+        'sport' => 'volleyball',
+        'occupation' => 'singer',
+        'status' => 'activated',
+    ],
+    [
+        'name' => 'Klara',
+        'birthYear' => 1988,
+        'hobby' => 'sport',
+        'sport' => 'volleyball',
+        'occupation' => 'programmer',
+        'status' => 'disabled',
+    ],
+    [
+        'name' => 'Juliusz',
+        'birthYear' => 1984,
+        'hobby' => 'sport',
+        'sport' => 'sqush',
+        'occupation' => 'programmer',
+        'status' => 'activated',
+    ],
+];
+
+$newUsers = [];
+
+$isNotHidden = function(array $user): bool {
+    return $user['status'] !== 'hidden';
+};
+
+$isNotDisabled = function(array $user): bool {
+    return $user['status'] !== 'disabled';
+};
+
+$isLessThan30 = function(array $user): bool {
+    return $user['birthYear'] >= 1987;
+};
+
+// pobranie aktywnych użytkowników mających mniej niż 30 lat
+$funcActivatedUsers = array_filter(array_filter(array_filter($users, $isNotHidden), $isNotDisabled), $isLessThan30);
+
+var_export($funcActivatedUsers);
