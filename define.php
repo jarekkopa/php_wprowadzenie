46f6a37efa4ee2002d65bc058a1811162bc0a974
Jarek <jarek.kopa@gmail.com>
2017-12-17 10:13:30 +0100

    Zadanie na definiowanie stałej z wyświetlaniem błędów w kodzie

---
+++ define.php
@@ -0,0 +1,10 @@
+<?php
+
+#wyświetlanie błędów
+error_reporting(E_ALL);
+ini_set('dispaly_errors', true);
+
+#treść zadania
+const MAX_SLIDES_NUMBER = 99;
+define("MAX_SLIDES_NUMBER", 666);
+echo "Maksymalna liczba slajdów: ", MAX_SLIDES_NUMBER, PHP_EOL;
