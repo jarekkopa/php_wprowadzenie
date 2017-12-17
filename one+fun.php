3b89dee3ae7ad8c8146b674c1095b2f20cd72341
Jarek <jarek.kopa@gmail.com>
2017-12-17 12:08:34 +0100

    Dodano komentarz do funkcji

--- one+fun.php
+++ one+fun.php
@@ -1,5 +1,11 @@
 <?php
-function printFamilyName($familyName)
+declare(strict_types=1);//zawsze tego używamy na początku pliku
+/**
+ * Wyświetlanie z formatowaniem podanego nazwiska
+ * 
+ * @param string $familyName nazwisko
+ */
+function printFamilyName(string $familyName): void
 {
     echo "Name: $familyName", PHP_EOL;
 }
