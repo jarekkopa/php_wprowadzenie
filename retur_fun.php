bd80f724ec5ee32495308c5d22f1acd1e50bb09f
Jarek <jarek.kopa@gmail.com>
2017-12-17 12:53:16 +0100

    Dodano dziania do obliczania

--- retur_fun.php
+++ retur_fun.php
@@ -1,9 +1,32 @@
 <?php
 declare(strict_types=1);
-
+/**
+ * Funkcja sumująca
+ * 
+ * @param int $a liczba a
+ * @param int $b liczba b
+ */
 function sum (int $a = 0, int $b = 0): int
 {
     return $a + $b;
 }
+function diff (int $a = 0, int $b = 0): int
+{
+    return $a - $b;
+}
+function multi (int $a = 0, int $b = 0): int
+{
+    return $a * $b;
+}
+function div (int $a = 0, int $b = 0): float
+{
+    if ($b === 0){
+        die("Nie dziel przez zero!");
+    }
+    return $a / $b;
+}
 
-echo sum(5, 3);
+echo sum(13, 5), PHP_EOL;
+echo diff(56, 25), PHP_EOL;
+echo multi(7, 8), PHP_EOL;
+echo div(3, 5), PHP_EOL;
