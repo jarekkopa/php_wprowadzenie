3944c49f8fb3a86af5932a6fd5a520cc91b37bac
Jarek <jarek.kopa@gmail.com>
2017-12-17 10:07:47 +0100

    Plik ze stałymi (const) w php

---
+++ const.php
@@ -0,0 +1,15 @@
+<?php
+echo __LINE__, PHP_EOL;
+echo __FILE__, PHP_EOL;
+echo __DIR__, PHP_EOL;
+echo __FUNCTION__, PHP_EOL;
+echo __CLASS__, PHP_EOL;
+echo __METHOD__, PHP_EOL;
+echo __NAMESPACE__, PHP_EOL;
+echo E_ALL, PHP_EOL;
+
+echo PHP_VERSION, PHP_EOL;
+echo PHP_OS, PHP_EOL;
+echo PHP_INT_MAX, PHP_EOL;
+echo DEFAULT_INCLUDE_PATH, PHP_EOL;
+
