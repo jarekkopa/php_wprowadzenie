9b525401ae056a7efe2c298aa1ab96d84fee19fe
Jarek <jarek.kopa@gmail.com>
2017-12-20 23:54:25 +0100

    Zadanie 1 - Pętla for, liczby od 1 do 10

---
+++ numbers.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Skrypt wyświetlający 10 liczb w kolejności od 1 do 10 przy użyciu pętli for.
+ */
+
+for ($i = 1; $i <= 10; ++$i) {
+    echo $i, PHP_EOL;
+}
