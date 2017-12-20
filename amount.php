03ab6ea3698c9189d26c52f40d18eed83f9443e5
Jarek <jarek.kopa@gmail.com>
2017-12-20 21:31:22 +0100

    Próba rozwiązania kolejnego probemu w zadaniu

--- amount.php
+++ amount.php
@@ -3,9 +3,20 @@
  * Skrypt wyświetlający kwotę z przedziału 0-999 na wartość słowną
  */
 
-$kwota = 20;
+$kwota = 10;
 
 $jednosci = ['Zero', 'Jeden', 'Dwa', 'Trzy', 'Cztery', 'Pięć', 'Sześć', 'Siedem', 'Osiem', 'Dziewięć'];
+$dziesiatki = ['dziesięć', 'dwadzieścia', 'trzydzieści', 'czterdzieści', 'pięćdziesiąt', 'sześćdziesiąt', 'siedemdziesiąt', 'osiemdziesiąt', 'dziewięćdziesiąt'];
 $zlotychJednosci = ['złotych', 'złoty', 'złote', 'złote', 'złote', 'złotych', 'złotych', 'złotych', 'złotych', 'złotych'];
 
-echo $jednosci[$kwota] . ' ' . $zlotychJednosci[$kwota];
+if ($kwota < 10) {
+    echo $jednosci[$kwota] . ' ' . $zlotychJednosci[$kwota];
+}
+
+if ($kwota  = 10){
+    var_export($kwota);
+    $kwotaStr = (string)$kwota;
+    $dziesiatki = $kwotaStr{0};
+    $ileJednosci = $kwotaStr{1};
+    echo $dziesiatki[$ileDziesiatki - 2], ' ' ;
+}
