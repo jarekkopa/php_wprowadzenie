efac7345ba3f79d1e402fba1aabc067e873ea38d
Jarek <jarek.kopa@gmail.com>
2018-01-14 13:19:53 +0100

    dodano wyświetlanie obiektów z metodą oreder()

--- index2.php
+++ index2.php
@@ -5,8 +5,9 @@ require_once 'AbstractProduct.php';
 require_once 'Hosting.php';
 require_once 'Domain.php';
 
-$hosting = new Hosting();
-$domain = new Domain();
-
-echo $hosting;
-echo $domain;
+$objectHosting = new Hosting();
+echo $objectHosting->order(), PHP_EOL;
+$objectDomain = new Domain();
+echo $objectDomain->order(), PHP_EOL;
+var_export($objectHosting);
+var_export($objectDomain);
