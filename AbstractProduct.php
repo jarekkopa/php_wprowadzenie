a82a245d367e0700e7d966a96fe35c075abe51ea
Jarek <jarek.kopa@gmail.com>
2018-01-14 12:57:38 +0100

    Uzupełnienie klasy abstrakcyjnej

--- AbstractProduct.php
+++ AbstractProduct.php
@@ -4,5 +4,17 @@ declare(strict_types = 1);
 
 abstract class AbstractProduct 
 {
+    protected $price;
+
+    abstract function order();
+
+    public function setPrice(int $price)
+    {
+        $this->price = $price;
+    }
     
+    public function getPrice()
+    {
+        return $this->price;
+    }
 }
