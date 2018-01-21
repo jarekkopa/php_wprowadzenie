59daf47da21e261ddff32a5c749405ec4515b305
Jarek <jarek.kopa@gmail.com>
2018-01-21 13:54:56 +0100

    Dodano pliki do src

---
+++ src/Jaro/Dishwasher.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jaro;
+
+class Dishwasher extends AbstractAgdDevice
+{
+    use PowerTrait;
+}
---
+++ src/Jaro/PowerTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jaro;
+
+trait PowerTrait
+{
+    protected $power;
+    
+    public function powerOff()
+    {
+        $this->power = "off";
+        return "Urządzenia wyłączono. Nadano status: " . $this->power;
+    }
+    public function powerOn()
+    {
+        $this->power = "on";
+        return "Urządzenia włączono. Nadano status: " . $this->power;
+    }
+}
