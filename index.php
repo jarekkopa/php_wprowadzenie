feb975ef3b848239db2ccd5ac84377a23fb0fd79
Jarek <jarek.kopa@gmail.com>
2018-01-21 14:40:27 +0100

    Dodano plik index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '\vendor\autoload.php';
+
+$dishwasher = new Jaro\Dishwasher();
+echo $dishwasher->powerOn(), PHP_EOL;
+echo $dishwasher->powerOff();
