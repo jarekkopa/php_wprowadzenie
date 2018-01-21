c4e69bdb4c5b54b7294c5193bc972952113d6085
Jarek <jarek.kopa@gmail.com>
2018-01-21 13:03:24 +0100

    Gotowe

--- PowerTrait.php
+++ PowerTrait.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 trait PowerTrait
 {
-        protected $power;
+    protected $power;
     
-        public function powerOff()
-        {
-            $this->power = "off";
-            return "Urządzenia wyłączono. Nadano status: " . $this->power;
-        }
-        public function powerOn()
-        {
-            $this->power = "on";
-            return "Urządzenia włączono. Nadano status: " . $this->power;
-        }
+    public function powerOff()
+    {
+        $this->power = "off";
+        return "Urządzenia wyłączono. Nadano status: " . $this->power;
+    }
+    public function powerOn()
+    {
+        $this->power = "on";
+        return "Urządzenia włączono. Nadano status: " . $this->power;
+    }
 }
