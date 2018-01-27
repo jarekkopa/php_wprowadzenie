fd168d1236595b9a05324249e359a7dcaa429ab4
Jarek <jarek.kopa@gmail.com>
2018-01-27 10:51:48 +0100

    Dodano 3 krok pobierajacy ujęcia z filmów

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
+// 1. Tworzy pojedynczy plik
 $poster = file_get_contents('https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg');
 
 file_put_contents('posters/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg', $poster);
 
+// 2. pobieramy plakaty
 for($i = 1; $i < 13; $i++)
 {
     $img = file_get_contents("https://cytaty.eu/img/sda/posters/".$i.".jpg");
     file_put_contents('posters/'.$i.".jpg", $img);
+}
+// 3. Pobieramy ujęcia
+for($i = 1; $i < 13; $i++)
+{
+    $img = file_get_contents("https://cytaty.eu/img/sda/shots/".$i.".jpg");
+    file_put_contents('shots/'.$i.".jpg", $img);
 }
