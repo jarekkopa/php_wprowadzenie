a0e21fab2bb6d132dc41dcc4aeae39deae029c5d
Jarek <jarek.kopa@gmail.com>
2018-01-27 12:09:41 +0100

    Gotowy plik z logami

--- index.php
+++ index.php
@@ -3,19 +3,33 @@
 declare(strict_types=1);
 
 // 1. Tworzy pojedynczy plik
-$poster = file_get_contents('https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg');
+// $poster = file_get_contents('https://upload.wikimedia.org/wikipedia/en/4/4f/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg');
 
-file_put_contents('posters/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg', $poster);
+// file_put_contents('posters/The_Hobbit_-_The_Desolation_of_Smaug_theatrical_poster.jpg', $poster);
 
 // 2. pobieramy plakaty
-for($i = 1; $i < 13; $i++)
+
+$log1 = 'Rozpoczęcie pobierania: ';
+$log2 = 'Zakoczenie pobierania: ';
+$date = date('Y-m-j h:i:s');
+
+for ($i=1; $i < 13; $i++)
 {
-    $img = file_get_contents("https://cytaty.eu/img/sda/posters/".$i.".jpg");
-    file_put_contents('posters/'.$i.".jpg", $img);
+    // $i - nazwa pod jaką jest zapisyany plik; $img - wskazuje jaki plik jest zapisywany
+    $imgType = "plakat";
+    $img = file_get_contents("https://cytaty.eu/img/sda/posters/". $i . ".jpg");
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1." ".$imgType.PHP_EOL,FILE_APPEND);
+    file_put_contents('posters/'.$i.".jpg", $img); 
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2." ".$imgType.PHP_EOL,FILE_APPEND);
 }
 // 3. Pobieramy ujęcia
 for($i = 1; $i < 13; $i++)
 {
+    $imgType = 'Shot';
     $img = file_get_contents("https://cytaty.eu/img/sda/shots/".$i.".jpg");
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1." ".$imgType.PHP_EOL,FILE_APPEND);
     file_put_contents('shots/'.$i.".jpg", $img);
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2." ".$imgType.PHP_EOL,FILE_APPEND);
 }
+
+$titles = ['Piraci z Karaibów', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars: Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka: Civil War', 'Spectre'];
