545c9c93f2d7aaa6acc97f791848fa80c5dcf432
Jarek <jarek.kopa@gmail.com>
2018-01-27 13:14:48 +0100

    Podaje listę plików na stronie www

--- index.php
+++ index.php
@@ -12,12 +12,15 @@ declare(strict_types=1);
 $log1 = 'Rozpoczęcie pobierania: ';
 $log2 = 'Zakoczenie pobierania: ';
 $date = date('Y-m-j h:i:s');
+$files = file_get_contents('https://cytaty.eu/img/sda/posters/');
+var_export($files);
+exit;
 
 $titles = ['Piraci z Karaibow', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka Civil War', 'Spectre'];
 
 for ($i=1; $i < 13; $i++)
 {
-    // $i - nazwa pod jaką jest zapisyany plik; $img - wskazuje jaki plik jest zapisywany
+    // $name - nazwa pod jaką jest zapisyany plik; $img - wskazuje jaki plik jest zapisywany
     $imgType = "plakat";
     $img = file_get_contents("https://cytaty.eu/img/sda/posters/". $i . ".jpg");
     file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
