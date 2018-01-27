0929fb5594899618e5243eda5315f25f1d1cb2c1
Jarek <jarek.kopa@gmail.com>
2018-01-27 12:28:55 +0100

    Dodanie nazwy pliku do logera

--- index.php
+++ index.php
@@ -13,23 +13,23 @@ $log1 = 'Rozpoczęcie pobierania: ';
 $log2 = 'Zakoczenie pobierania: ';
 $date = date('Y-m-j h:i:s');
 
+$titles = ['Piraci z Karaibów', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars: Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka: Civil War', 'Spectre'];
+
 for ($i=1; $i < 13; $i++)
 {
     // $i - nazwa pod jaką jest zapisyany plik; $img - wskazuje jaki plik jest zapisywany
     $imgType = "plakat";
     $img = file_get_contents("https://cytaty.eu/img/sda/posters/". $i . ".jpg");
-    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1." ".$imgType.PHP_EOL,FILE_APPEND);
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
     file_put_contents('posters/'.$i.".jpg", $img); 
-    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2." ".$imgType.PHP_EOL,FILE_APPEND);
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
 }
 // 3. Pobieramy ujęcia
 for($i = 1; $i < 13; $i++)
 {
     $imgType = 'Shot';
     $img = file_get_contents("https://cytaty.eu/img/sda/shots/".$i.".jpg");
-    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1." ".$imgType.PHP_EOL,FILE_APPEND);
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
     file_put_contents('shots/'.$i.".jpg", $img);
-    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2." ".$imgType.PHP_EOL,FILE_APPEND);
+    file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
 }
-
-$titles = ['Piraci z Karaibów', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars: Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka: Civil War', 'Spectre'];
