fb5a829f7f5f90f06c07553747f6cd2c1f0d1fe9
Jarek <jarek.kopa@gmail.com>
2018-01-27 12:48:16 +0100

    Dodanie zamiany nazw plików ze zdjęciami na tekst a nie cyfry

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $log1 = 'Rozpoczęcie pobierania: ';
 $log2 = 'Zakoczenie pobierania: ';
 $date = date('Y-m-j h:i:s');
 
-$titles = ['Piraci z Karaibów', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars: Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka: Civil War', 'Spectre'];
+$titles = ['Piraci z Karaibow', 'Ring', 'Blade Runner 2049', 'Thor', 'Get Out', 'Star Wars Last Jedi', 'Okja', 'London', 'Tarzan', 'The Founder', 'Kapitan Ameryka Civil War', 'Spectre'];
 
 for ($i=1; $i < 13; $i++)
 {
@@ -21,7 +21,8 @@ for ($i=1; $i < 13; $i++)
     $imgType = "plakat";
     $img = file_get_contents("https://cytaty.eu/img/sda/posters/". $i . ".jpg");
     file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
-    file_put_contents('posters/'.$i.".jpg", $img); 
+    $name = str_ireplace(' ', '_', strtolower($titles[$i-1]));
+    file_put_contents('posters/'.$name.".jpg", $img); 
     file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
 }
 // 3. Pobieramy ujęcia
@@ -30,6 +31,7 @@ for($i = 1; $i < 13; $i++)
     $imgType = 'Shot';
     $img = file_get_contents("https://cytaty.eu/img/sda/shots/".$i.".jpg");
     file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log1. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
-    file_put_contents('shots/'.$i.".jpg", $img);
+    $name = str_ireplace(' ', '_', strtolower($titles[$i-1]));
+    file_put_contents('shots/'.$name.".jpg", $img);
     file_put_contents('logs/filmoteka.log', " Data: ".$date." - ".$log2. $titles[$i-1]." - ".$imgType.PHP_EOL,FILE_APPEND);
 }
