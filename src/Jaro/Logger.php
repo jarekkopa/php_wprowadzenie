861915a402595e1c3e68bf15e56b03562b050c9d
Jarek <jarek.kopa@gmail.com>
2018-01-28 10:11:44 +0100

    Stworzono działającą klasę Logger

--- src/Jaro/Logger.php
+++ src/Jaro/Logger.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Jaro;
+
+class Logger
+{
+    public function saveToLogs($log = 'test')
+    {
+        file_put_contents('logs/filmoteka.log', date('Y-m-j h:i:s') . " " . $log . PHP_EOL,FILE_APPEND);
+    }
+}
+// $loog = new Logger();
+// echo $loog->saveToLogs();
